83d33e8a0ded51fb014e11774d708c2695a86994
“Jason <jasongxu01@gmail.com>
2025-08-14 23:29:34 -0400

    export types

--- src/api/src/index.ts
+++ src/api/src/index.ts
@@ -1,2 +1,7 @@
 export { createRelayClient } from './createRelayClient';
 export { RelayEnvironment } from './RelayEnvironment';
+export type {
+  paths as RelayPaths,
+  components as RelayComponents,
+  operations as RelayOperations,
+} from './openapi/types';
